f7f41739c3097df85a5d1a1ce621278d16285d73
cesarvalentimjr <cesarvalentimjr@gmail.com>
2025-09-25 10:31:50 -0300

    Delete test_docling_mlgsan.py

--- test_docling_mlgsan.py
+++
@@ -1,7 +0,0 @@
-from docling.document_converter import DocumentConverter
-
-converter = DocumentConverter()
-result = converter.convert("/home/ubuntu/upload/MLGSAN122024.pdf")
-
-print(result.document.export_to_markdown())
-
